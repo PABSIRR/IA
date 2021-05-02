8b37443b7fde2c10374af98eb25f161e6193ff3e
PABSIRR <pavan.gudavalli1@gmail.com>
2021-05-02 15:51:31 -0600

    final changes(prob)

--- IA/settings.py
+++ IA/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'IA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'IA/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
