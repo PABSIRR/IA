8867e06b2effb48a44ccb66f159228cd37bb444f
PABSIRR <pavan.gudavalli1@gmail.com>
2021-05-02 18:00:50 -0600

    rejew

--- Users/urls.py
+++ Users/urls.py
@@ -7,9 +7,9 @@ app_name = "users"
 
 urlpatterns = [
     #Login
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='Users/login.html'), name='login'),
     #Logout
-    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
     #Register
     path('register/', views.register, name='register'),
 ]
